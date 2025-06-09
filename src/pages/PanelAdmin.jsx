85f04acfada7370732700370024df393d1979bc5
unknown <carrilloerlan@gmail.com>
2025-06-09 17:29:38 -0400

    Admin1.2.9

--- src/pages/PanelAdmin.jsx
+++ src/pages/PanelAdmin.jsx
@@ -4,82 +4,85 @@ function PanelAdmin() {
   return (
     <motion.div>
       <h1>Panel de administrador</h1>
-      <h2>Agregar un nuevo evento</h2>
-      <motion.div className="form-group">
-        <label className="form-label">Nombre del evento</label>
-        <input
-          type="text"
-          name="nombre_evento"
-          className="form-input"
-        />
-        <label className="form-label">Fecha</label>
-        <input
-          type="date"
-          name="fecha_evento"
-          className="form-input"
-        />
-        <label className="form-label">Hora</label>
-        <input
-          type="time"
-          name="hora_evento"
-          className="form-input"
-        />
-        <label className="form-label">Ubicación</label>
-        <input
-          type="text"
-          name="ubicacion_evento"
-          className="form-input"
-        />
-        <label className="form-label">Descripción</label>
-        <input
-          type="text"
-          name="descripcion_evento"
-          className="form-input"
-        />
+      <motion.div>
+        <h2>Agregar un nuevo evento</h2>
+        <motion.div className="form-group">
+          <label className="form-label">Nombre del evento</label>
+          <input
+            type="text"
+            name="nombre_evento"
+            className="form-input"
+          />
+          <label className="form-label">Fecha</label>
+          <input
+            type="date"
+            name="fecha_evento"
+            className="form-input"
+          />
+          <label className="form-label">Hora</label>
+          <input
+            type="time"
+            name="hora_evento"
+            className="form-input"
+          />
+          <label className="form-label">Ubicación</label>
+          <input
+            type="text"
+            name="ubicacion_evento"
+            className="form-input"
+          />
+          <label className="form-label">Descripción</label>
+          <input
+            type="text"
+            name="descripcion_evento"
+            className="form-input"
+          />
+        </motion.div>
+        <motion.button
+          className="btn btn-primary"
+          whileTap={{ scale: 0.95 }}
+          style={{ marginTop: "1.5rem" }}
+        >
+          Publicar evento
+        </motion.button>
       </motion.div>
-      <motion.button
-        className="btn btn-primary"
-        whileTap={{ scale: 0.95 }}
-        style={{ marginTop: "1.5rem" }}
-      >
-        Publicar evento
-      </motion.button>
 
-      
-      <h2>Agregar un nuevo anuncio</h2>
-      <motion.div className="form-group">
-        <label className="form-label">Titulo del anuncio</label>
-        <input
-          type="text"
-          name="nombre_anuncio"
-          className="form-input"
-        />
-        <label className="form-label">Descripción</label>
-        <input
-          type="text"
-          name="descripcion_anuncio"
-          className="form-input"
-        />
-        <label className="form-label">Fecha</label>
-        <input
-          type="date"
-          name="fecha_anuncio"
-          className="form-input"
-        />
-        <label className="form-label">Hora</label>
-        <input
-          type="time"
-          name="hora_anuncio"
-          className="form-input"
-        />
+      <motion.div>
+        <h2>Agregar un nuevo anuncio</h2>
+        <motion.div className="form-group">
+          <label className="form-label">Titulo del anuncio</label>
+          <input
+            type="text"
+            name="nombre_anuncio"
+            className="form-input"
+          />
+          <label className="form-label">Descripción</label>
+          <input
+            type="text"
+            name="descripcion_anuncio"
+            className="form-input"
+          />
+          <label className="form-label">Fecha</label>
+          <input
+            type="date"
+            name="fecha_anuncio"
+            className="form-input"
+          />
+          <label className="form-label">Hora</label>
+          <input
+            type="time"
+            name="hora_anuncio"
+            className="form-input"
+          />
+        </motion.div>
+        <motion.button
+          className="btn btn-primary"
+          whileTap={{ scale: 0.10 }}
+          style={{ marginTop: "1rem" }}
+        >
+          Publicar anuncio
+        </motion.button>
       </motion.div>
-      <motion.button
-        className="btn btn-primary"
-        whileTap={{ scale: 0.95 }}
-        style={{ marginTop: "1.5rem" }}
-      >
-        Publicar anuncio
-      </motion.button>
     </motion.div>
   )
 };
