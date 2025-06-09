f3121ccbcedfec33625cf5708f41375bf8744802
unknown <carrilloerlan@gmail.com>
2025-06-09 16:35:09 -0400

    Admin1.2.2

--- src/pages/PanelAdmin.jsx
+++ src/pages/PanelAdmin.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 function PanelAdmin() {
   return (
-    <h1>Panel de admin</h1>
+    <motion.div>
+      <h1>Panel de administrador</h1>
+      <h2>Agrega un evento</h2>
+      <motion.div className="form-group">
+        <label className="form-label">Nombre</label>
+        <input
+          type="text"
+          name="nombre"
+          className="form-input"
+      />
+      </motion.div>
+      <h2>Agrega un anuncio</h2>
+      <motion.div className="form-group">
+        <label className="form-label">Nombre</label>
+        <input
+          type="text"
+          name="nombre"
+          className="form-input"
+      />
+      </motion.div>
+    </motion.div>
   )
-}
+};
 
 export default PanelAdmin;
