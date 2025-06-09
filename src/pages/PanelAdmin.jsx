5491cdf856cba1519946f411b765f80bc5f7a619
unknown <carrilloerlan@gmail.com>
2025-06-09 16:18:35 -0400

    Admin1.2.0

--- src/pages/PanelAdmin.jsx
+++ src/pages/PanelAdmin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 function PanelAdmin() {
   return (
     <div className="max-w-4xl mx-auto p-6 bg-white rounded-xl shadow-lg mt-10">
